32230731fb7a5e28d54a5a884aae42dbf3fedf19
Sean Franey <s_franey@hotmail.com>
2022-04-03 14:20:44 -0700

    Currently unsuccessful attempt to serve using unused port 1315 rather than 1313

--- scripts/docker_spin.sh
+++ scripts/docker_spin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -e TZ=America/Los_Angeles --net=host -v /home/sean/Documents/blogs/technology-blog:/blogs --user 1000:1000 hugo-alpine:1.0 $1
+docker run --rm -e TZ=America/Los_Angeles -v /home/sean/Documents/blogs/technology-blog:/blogs --user 1000:1000 -p 1315:1313 hugo-alpine:1.0 $1 --bind 0.0.0.0
